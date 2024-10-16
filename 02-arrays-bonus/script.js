f56765678cc8f000ae55c899a39c0b3e17e23b06
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-10-16 17:35:25 +0200

    add

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,12 +10,16 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers =[];
+const reversedTeachers=[];
+const reversedTeachersBonus2=[];
 for (let i=0;i<teachers.length;i++) {
   reversedTeachers.push(teachers.at([-i-1]));
 }
+for (let i=teachers.length-1;i>=0; i--) {
+  reversedTeachersBonus2.push(teachers[i]);
+}
 const reversedTeachersBonus = teachers.slice().reverse();
-console.log(reversedTeachers,reversedTeachersBonus);
+console.log(reversedTeachers,reversedTeachersBonus,reversedTeachersBonus2);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
@@ -29,5 +33,7 @@ const longNamesBonus = teachers.filter(teacher => teacher.length >= 5);
 console.log(longNames,longNamesBonus);
 
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(1, 1);
-console.log(teachers);
+const newTeachers =teachers.slice();
+newTeachers.splice(1, 1);
+const newTeachersBonus = teachers.filter(item => item  !== "Ed");
+console.log(newTeachers,newTeachersBonus,teachers);
